6fc23a03a38742af56c9c77c14d3f263f2a2259f
sdebowski <s_debowski@wp.pl>
2024-07-21 19:48:36 +0200

    Wykonanie zadania

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let cities = ['Warsaw', 'Berlin', 'Paris', 'London', 'New York', 'Tokio', 'Moscow'];
+
+let ul = document.createElement('ul');
+
+for (let i = 0; i < cities.length; i++) {
+    let li = document.createElement('li');
+    li.classList.add('city');
+    li.textContent = cities [i];
+    ul.appendChild(li);
+}
+
+document.body.appendChild(ul);
